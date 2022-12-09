b5a86fafdde4a401a85b16e1de24be9005da9a20
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-09 16:24:09 -0300

    criando rota POST

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -8,4 +8,9 @@ router.get('/checklists', (req, res) => {
   res.json({ title: 'não se atrasar', isCompleted: true });
 });
 
+router.post('/checklists', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+});
+
 module.exports = router;
