d2f4b27b5033aa9b30319f714912485b50fd3ed7
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-09 16:40:07 -0300

    criação das rotas PUT e DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -8,6 +8,7 @@ router.get('/checklists', (req, res) => {
   res.json({ title: 'não se atrasar', isCompleted: true });
 });
 
+//cadastra o que está no body.
 router.post('/checklists', (req, res) => {
   console.log(req.body);
   res.status(200).send(req.body);
@@ -18,6 +19,16 @@ router.post('/checklists', (req, res) => {
 router.get('/checklists/:id', (req, res) => {
   console.log(req.params.id);
   res.send(`Id ${req.params.id}`);
+});
+
+router.put('/checklists/:id', (req, res) => {
+  console.log(req.body);
+  res.send(`Put ${req.params.id}`);
+});
+
+router.delete('/checklists/:id', (req, res) => {
+  console.log(req.body);
+  res.send(`Put ${req.params.id}`);
 });
 
 module.exports = router;
