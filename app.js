6b57e852dac2725064bce2dc142b17ac85e0c7d7
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-12 17:45:57 -0300

    adicionando arquivos estaticos

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 //me permite enviar objeto json
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('views engine', 'ejs');
