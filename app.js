6e59b2bb68c82a2169c84f41e00be1bdb8b5640e
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-12 14:39:21 -0300

    pegando o erro da promise de conexao

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const checklistRouter = require('./src/routes/checklist');
 const express = require('express');
+const db = require('./config/db.config');
 
 //app recebendo o express com o seus metodos
 const app = express();
 
 //me permite enviar objeto json
 app.use(express.json());
+
+//resposta da promise
+
+db.on('error', console.log.bind(console, 'Erro ao conectar com o banco'));
+
+db.once('open', () => {
+  console.log('Conexão com o banco feito com sucesso');
+});
 
 //usando a rota
 app.use(checklistRouter);
