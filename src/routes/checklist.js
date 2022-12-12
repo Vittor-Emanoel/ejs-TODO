a35b7ba3743de9c55b9af5887c0079ceeb7fa402
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-12 17:18:16 -0300

    atualização e remoção

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,6 +1,6 @@
 // começando a primeira rota
 const express = require('express');
-const mongoose = require('mongoose');
+
 const router = express.Router();
 
 const Checklist = require('../models/checklist.js');
@@ -23,7 +23,7 @@ router.post('/checklists', async (req, res) => {
     res.status(200).json(checklist);
   } catch (error) {
     if (!name) {
-      res.status(422).json(`Nome não foi encontrado`);
+      res.status(400).json(`Nome não foi encontrado`);
     } else {
       res.status(422).json(`Erro ao cadastrar tarefa : ${error}`);
     }
@@ -42,14 +42,28 @@ router.get('/checklists/:id', async (req, res) => {
   res.send(`Id ${req.params.id}`);
 });
 
-router.put('/checklists/:id', (req, res) => {
-  console.log(req.body);
-  res.send(`Put ${req.params.id}`);
+//atualizando
+router.put('/checklists/:id', async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    );
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(`Erro ao atualizar a checklist ${error}`);
+  }
 });
 
-router.delete('/checklists/:id', (req, res) => {
-  console.log(req.body);
-  res.send(`Put ${req.params.id}`);
+router.delete('/checklists/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    res.status(204).json(checklist);
+  } catch (error) {
+    res.status(500).json(`Erro ao buscar ao checklist ${error}`);
+  }
 });
 
 module.exports = router;
